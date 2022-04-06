e9246e2db58acd51791665cccdb804d7f071866d
goodvibrations32 <87814811+goodvibrations32@users.noreply.github.com>
2022-04-06 15:27:08 +0300

    Rename files and folders (#3)
    * Adding two methods of filtering with same filter on Higher and Lower Order (number of coefficients)
    
    * Fastest approach but Bode Plots are plotting with no errors
    
    * spell correction for jupyter notebook integration when importing
    
    * Delete CSV read method. The given data are in .h5 file
    
    * Addition of dataframe creation files for 2/3 approaches
    
    * gitignore edit for the data folder in local disk
    
    * adding h5 create process for results
    
    * Create and read new h5 file
    
    * working with not specified shape
    
    * Update dataFrame structure for more info in the file
    
    * create new h5 file for results
    
    * Addition of user defined name and file path for raw and filtered
    
    * improved python syntax and utilize of python functions
    
    * Addition of __name__ function for code shrinking (hudge line/ job ratio impact)
    
    * rename properly
    
    * delete space on the end

---
+++ src/signal_process_plots_datasets/High_resolution_filter_/create_dataset_filt_high_res_.py
@@ -0,0 +1,52 @@
+#%%
+
+import numpy as np
+import pandas as pd
+from scipy import signal
+from Plots_time_freq_domain_ import UNCORR, TIME_NO_SHIFT, L, file_name_of_raw, file_path
+
+#Construct data frame better 
+#using pandas library
+
+#Rename old attributes to match with filtered data signal
+F=[]
+for item in L:
+    F.append(item.replace("raw","filt")) 
+
+#Create a dictionary from the lists of modified keys and filtered output 
+a = dict(zip(F, UNCORR))
+
+#Create a pandas dataframe for the generated file 
+df=pd.DataFrame(data=a, index=None)
+df.insert(loc=0, column="Time", value=TIME_NO_SHIFT)
+
+#Create the new file
+#Choose the desired directory to create the new file 
+#and add the __NAME__.h5 at the end of the screen 
+# WARNING : If file already exists in the dir and it is closed via :
+#hf_st_pd_.close() it will be overwritten
+
+new_file_name = input("""Enter the name of the new file : 
+the file will be created in the same path folder with the raw data 
+if 0 is passed for a new name the old name is used with replacing
+the "raw" at the start of the name with "HD"
+""")
+
+if new_file_name == '0' :
+    file_name = file_name_of_raw.replace("raw", "HD")
+else:
+    file_name = new_file_name
+
+#HDF5 file in same path with raw signal folder diff name
+hf_st_pd_ = pd.HDFStore(f'{file_path}{file_name}', mode='w')
+hf_st_pd_.put('df_filt', df, format='table', data_columns=True)
+hf_st_pd_.close()
+#%%
+#Read the file that was just created
+
+f_3 = pd.HDFStore(path=f'{file_path}{file_name}',mode='r')
+
+data_filt = f_3['df_filt']
+# %%
+#This is added in order to avoid manual close of the filtered data file
+f_3.close()
---
+++ src/signal_process_plots_datasets/Low_resolution_filter_/test__name__func.py
@@ -0,0 +1,6 @@
+#%%
+import plots_time_freq_domain_dirty_
+from plots_time_freq_domain_dirty_ import MATRIX_RAW
+
+
+# %%
