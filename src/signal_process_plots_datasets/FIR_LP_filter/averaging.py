3ef1473a12df1dcd679bba32574158c5d244633d
N. Papadakis (d-g5-5590) <npapnet@gmail.com>
2022-06-04 22:21:35 +0300

    Significant changes to the Class WT_Noise_ChannelProcessor_averaging. - Added factory method from tdms - Added factory method from obj - rewrote init (Added operations list argument which records the operation) - changed behaviour to allow for method chaining - rewrote filter to allow easily external filters.
    Created a Filter Function Factory method
    At some point I need to move this function to another file.

--- src/signal_process_plots_datasets/FIR_LP_filter/averaging.py
+++ src/signal_process_plots_datasets/FIR_LP_filter/averaging.py
@@ -19,7 +19,6 @@
 #%%
 from pathlib import Path
 from matplotlib import pyplot as plt
-import matplotlib
 import scipy.signal as signal
 import numpy as np
 import pandas as pd
@@ -27,42 +26,74 @@ import pandas as pd
 import nptdms
 from nptdms import TdmsFile
 
-from functions import spect, plot_spect_comb2, Graph_data_container
-from signal_process_plots_datasets.FIR_LP_filter.decim_Torque import WT_Noise_ChannelProcessor
+from functions import spect,  plot_spect_comb2, Graph_data_container
+import logging
+logging.basicConfig( level=logging.WARNING)
 
 #%% Imported from raw_signal_Comp2
 
 
 #%% Functions and classes
-def apply_filter(ds:np.ndarray, fs_Hz:float, fc_Hz = 100, filt_order = 2 ):
-                 # filter cutoff frequency
-    sos = signal.butter(filt_order , fc_Hz, 'lp', fs=fs_Hz, output='sos')
-    filtered = signal.sosfilt(sos, ds-ds[0])+ds[0]
-    return filtered
-
-
-
-class WT_Noise_ChannelProcessor_averaging():
-    #TODO Consider using class methods to implement factory methods. (from tdms or ) 
-    def __init__(self, tdms_channel:nptdms.tdms.TdmsChannel, desc:str) -> None:
-        """_summary_
+def filt_butter_factory( filt_order = 2):
+    """this is a factory method that produces a BUTTERWORTH filter function 
+    with a filter order and a cutoff frequency
+    
+    Args:
+        filt_order (int, optional): Filter order. Defaults to 2.
+        #TODO condider using also the fc_Hz as an argument so that the only parameters are:
+        # the data and sampling frequnecy 
+    """    
+    def filt_butter(ds:np.ndarray, fs_Hz:float, fc_Hz:float, filt_order = filt_order ):
+                    # filter cutoff frequency
+        """applies filtering to np.array with fs_Hz data, with cuffoff frequency
 
         Args:
-            tdms_channel (nptdms.tdms.TdmsChannel): tmds channel 
-            desc (str): descirption (used)
-        """        
-        self._channel_data= tdms_channel
+            ds (np.ndarray): _description_
+            fs_Hz (float): _description_
+            fc_Hz (int, optional): cutoff frequency of filter (low pass).Defaults to 100.
+            filt_order (int, optional): _description_. Defaults to 2.
+
+        Returns:
+            _type_: _description_
+        """                 
+        sos = signal.butter(filt_order , fc_Hz, 'lp', fs=fs_Hz, output='sos')
+        filtered = signal.sosfilt(sos, ds-ds[0])+ds[0]
+        return filtered
+    return filt_butter
+filter_Butter_default=filt_butter_factory(filt_order = 2)
+
+class WT_Noise_ChannelProcessor_averaging():
+    """
+    Class for processing a tdms file
+    """
+    #TODO Consider using class methods to implement factory methods. (from tdms or ) 
+    __measurement_history = []
+    
+    def __init__(self, desc:str, fs_Hz:float, data:np.ndarray, 
+                 channel_name:str, group_name:str, _channel_data,
+                 operations:list):
+        self._channel_data= _channel_data
         self.set_description(desc=desc)
         # process details
-        self.fs_Hz = 1/self._channel_data.properties['wf_increment']
-        self.data = self._channel_data.data
-        self.channel_name = self._channel_data.name
-        self.group_name = self._channel_data.group_name
-            
-    def set_description(self, desc):
-        self.description = desc
+        self.fs_Hz = fs_Hz
+        self.data = data
+        self.channel_name = channel_name
+        self.group_name = group_name
+        self.__measurement_history = operations
         
-    def raw_data(self):
+    def set_description(self, desc):
+        """sets the description of the file
+
+        Args:
+            desc (_type_): _description_
+        """        
+        self.description = desc
+    @property
+    def operations(self):
+        return self.__measurement_history
+     
+    @property
+    def data_as_Series(self)->pd.Series:
         """returns the raw data as a pd.Series
 
         Returns:
@@ -70,7 +101,7 @@ class WT_Noise_ChannelProcessor_averaging():
         """        
         return pd.Series(self.data, name=f'{self.channel_name}:raw')
     
-    def get_decimated(self, dec:int, offset:int) -> dict:
+    def get_decimated(self, dec:int, offset:int=0):
         """returns a decimated data seires
 
         Args:
@@ -82,33 +113,49 @@ class WT_Noise_ChannelProcessor_averaging():
         """        
         decimated_fs_Hz = self.fs_Hz/dec
         
-        return {fs_Hz:decimated_fs_Hz, 'data': self.data[offset::dec]}
+        new_operation = f'Decimation factor:{dec}, Offset:{offset}, new fs_Hz:{decimated_fs_Hz}'
+        return WT_Noise_ChannelProcessor_averaging.from_obj(self,
+                operation=new_operation,  
+                fs_Hz= decimated_fs_Hz, data=self.data[offset::dec])
     
-    def filter(self, fc_Hz:float, fs_Hz:None):
-        """performs 
+    def filter(self, fc_Hz:float, filter_func=filter_Butter_default, fs_Hz=None )->pd.Series:
+        """return a filtered signal based on 
 
         Args:
-            fc_Hz (float): cutoff frequency in Hz
-            fs_Hz (float): sampling frequency in Hz
-
+            fc_Hz (float): cut off frequency in Hz
+            filter_func (filt_butter_factory, optional): filtering function that thates two arguments (ds, fs_Hz). Defaults to 100, filt_order = 2).
+            fs_Hz (None): sampling frequency in Hz (#TODO SHOULD BE REMOVED)
+            
         Returns:
             _type_: _description_
-        """        
+        """    
+        if fs_Hz is not None:
+            logging.warning(f'issued {fs_Hz} sampling frequency while the default is {self.fs_Hz}')
         fs_Hz = fs_Hz if fs_Hz is not None else self.fs_Hz
-        filtered = apply_filter(ds=self.data, fs_Hz=fs_Hz, fc_Hz=fc_Hz )
+        filtered = filter_func(ds=self.data, fs_Hz=fs_Hz, fc_Hz=fc_Hz )
         return pd.Series(filtered, name=f'{self.channel_name}:filt_fc_{fc_Hz}')
     
-    def get_spectrum_raw(self)->Graph_data_container:
+    def filter_as_obj(self,  fc_Hz:float, filter_func=filter_Butter_default, fs_Hz=None, desc=None):
+        ds_filt = self.filter( fc_Hz=fc_Hz, filter_func=filter_func, fs_Hz=fs_Hz )
+        description = desc if desc is not None else self.description + f"_fc:{fc_Hz}"
+        return WT_Noise_ChannelProcessor_averaging.from_obj(self, 
+            desc = description,
+            data = ds_filt.values,
+            operation = f'pass filter {fc_Hz}'
+            )
+    
+    def get_spectrum_raw(self,window='flattop', nperseg=1_024, scaling='spectrum')->Graph_data_container:
         """returns a Graph_data_container object with the power spectrum of the data. 
+        uses the decimation function
 
         Returns:
             _type_: _description_
         """        
-        x_r,y_r = spect(self.data, FS=self.fs_Hz)        
-        return Graph_data_container(x=x_r,y = y_r, label = f'{self.description}-{self.channel_name}')
+        gobj = self.get_spectrum_raw_dec(dec=1, window=window,nperseg= nperseg, scaling=scaling)     
+        return gobj
     
     
-    def get_spectrum_raw_dec(self, dec:int, offset:int=0)->Graph_data_container:
+    def get_spectrum_raw_dec(self, dec:int=1, offset:int=0, window='flattop', nperseg=1_024, scaling='spectrum')->Graph_data_container:
         """returns a Graph_data_container object with the power spectrum of the **decimated** data. 
 
         Args:
@@ -119,12 +166,16 @@ class WT_Noise_ChannelProcessor_averaging():
             Graph_data_container: _description_
         """        
         decimated_fs_Hz = self.fs_Hz/dec
-        x_r,y_r = spect(self.data[offset::dec], FS=decimated_fs_Hz)        
+        x_r,y_r = spect(self.data[offset::dec], FS=decimated_fs_Hz,window=window,nperseg= nperseg, scaling=scaling)        
+        if dec==1:
+            label = f'{self.description}-{self.channel_name}'
+        else:
+            label = f'{self.description}-{self.channel_name}-fs:{decimated_fs_Hz/1000:.1f}kHz '
         return Graph_data_container(x=x_r,y = y_r, 
-            label = f'{self.description}-{self.channel_name}-fs:{decimated_fs_Hz/1000:.1f}kHz ')
+            label = label)
     
     
-    def get_spectrum_filt(self, fc_Hz:float)->Graph_data_container:
+    def get_spectrum_filt(self, fc_Hz:float, filt_func=filter_Butter_default)->Graph_data_container:
         """returns a Graph_data_container object with the power spectrum of the data. 
 
         Args:
@@ -133,26 +184,40 @@ class WT_Noise_ChannelProcessor_averaging():
         Returns:
             Graph_data_container: _description_
         """        
-        x_f,y_f = spect(self.filter(fc_Hz=fc_Hz), FS=self.fs_Hz)        
+        x_f,y_f = spect(self.filter(fc_Hz=fc_Hz, filter_func=filt_func), FS=self.fs_Hz)        
         return Graph_data_container(x=x_f,y = y_f, label = f'{self.description}-{self.channel_name} - filt: {fc_Hz}')
     
     def plot_filtered_th(self,fc_Hz):
+        """plots the Time History
+
+        Args:
+            fc_Hz (_type_): _description_
+        """        
         plt.plot(self.data, label = 'raw')
         plt.plot(self.filter(fc_Hz=fc_Hz), label = f'filtered: {fc_Hz}')
 
-    def get_averaged(self, fr_Hz:100):
-        """returns 
+    def get_averaged(self, fr_Hz:float=100, desc:str= None):
+        """returns another object 
 
         Args:
-            fr_Hz (100): recording frequency (with averaging)
+            fr_Hz (float) : recording frequency (with averaging)
+            desc (str) : description (Defaults to None: ie. add suffix _Av:<fr_Hz>)
         """        
-        ds = self.raw_data()
+        ds = self.data_as_Series
         fs_Hz = self.fs_Hz
-        fr_Hz = 100
-    
-        ds_fr = ds.groupby(ds.index// (fs_Hz/fr_Hz)).mean()
+            
+        ds_fr = ds.groupby(ds.index// (fs_Hz/fr_Hz)).mean().values
+        
+        description = desc if desc is not None else self.description + f"_Av:{fr_Hz}"
         #TODO use factory method to return a new object. 
-        return ds 
+        new_operations = self.operations.copy()
+        new_operations.append(f'Apply Averaging :{fr_Hz}')
+        return WT_Noise_ChannelProcessor_averaging(desc = description
+                , fs_Hz=fr_Hz, data = ds_fr, 
+                channel_name=self.channel_name, 
+                group_name=self.group_name, _channel_data = None,
+                operations = new_operations
+                )
     
     @classmethod
     def from_tdms(cls, tdms_channel:nptdms.tdms.TdmsChannel, desc:str):
@@ -164,13 +229,39 @@ class WT_Noise_ChannelProcessor_averaging():
             desc (str): descirption (used)
         """        
         _channel_data= tdms_channel
-        desc
+        # desc
 
         fs_Hz = 1/_channel_data.properties['wf_increment']
         data = _channel_data.data
         channel_name =_channel_data.name
         group_name = _channel_data.group_name
-        return cls(desc, fs_Hz, data, channel_name, group_name, _channel_data)
+         
+        return cls(desc, fs_Hz, data, channel_name, group_name, _channel_data, 
+                   operations=[f'Loaded from tdms file {group_name}/{channel_name}, {fs_Hz}'])
+    
+    @classmethod
+    def from_obj(cls, obj, operation:str=None, **kwargs):
+        """Factory method that generates an object class
+        if parameters are not provided the obj values are used. 
+        
+        Args:
+            tdms_channel (nptdms.tdms.TdmsChannel): tmds channel 
+            desc (str): descirption (used)
+        """
+        assert isinstance(operation, str)
+        # the following line allows to pass a description maybe
+        desc = obj.description if kwargs.get('desc', None) is None else kwargs.get('desc', None)
+        
+        _channel_data = kwargs.get('_channel_data', obj._channel_data)
+        fs_Hz = kwargs.get('fs_Hz', obj.fs_Hz)
+        data = kwargs.get('data', obj.data)
+        channel_name = kwargs.get('channel_name', obj.channel_name)
+        group_name = kwargs.get('group_name', obj.group_name)
+        
+        new_ops = obj.operations.copy()
+        new_ops.append(operation)
+        return cls(desc, fs_Hz, data, channel_name, group_name, _channel_data, 
+            operations=new_ops)
 #%%[markdown]
 #
 # only the 100 kHz signal is required.  
@@ -189,13 +280,13 @@ tdms_f_name = 'Data.tdms'
 tdms_folder_id = 'WTmeas20220512-'
 
 #%% [markdown]
-
+#%% CONSTANTS
 
 GROUP_NAME = 'Wind Measurement'
 CHAN_NAME = 'Torque'
 
 #%%
-# Inverter measurments 
+# Inverter measurements 
 # Dir name 
 inv_meas_dir = 'Inverter'
 
@@ -210,7 +301,7 @@ path_inv_meas = FOLDER_FOR_DATA / inv_meas_dir / f'{tdms_folder_id}{WT_inv_1_WS_
 
 tdms_raw_WT =TdmsFile(path_inv_meas)
 
-df_tdms_inv_meas_1_0 = WT_Noise_ChannelProcessor_averaging(tdms_raw_WT[GROUP_NAME][CHAN_NAME]
+df_tdms_inv_meas_1_0 = WT_Noise_ChannelProcessor_averaging.from_tdms(tdms_raw_WT[GROUP_NAME][CHAN_NAME]
                 , desc= 'Inverter On, WS=0, 100kHz')
 #%%
 
@@ -227,37 +318,81 @@ tdms_raw_WT_50kHz =TdmsFile(path_dec_meas_50_kHz)
 tdms_raw_WT_5kHz =TdmsFile(path_dec_meas_5_kHz)
 
 
-df_tdms_dec_50kHz = WT_Noise_ChannelProcessor_averaging(tdms_raw_WT_50kHz[GROUP_NAME][CHAN_NAME]
+df_tdms_dec_50kHz = WT_Noise_ChannelProcessor_averaging.from_tdms(tdms_raw_WT_50kHz[GROUP_NAME][CHAN_NAME]
                 , desc= 'Inverter On, WS=0, 50kHz')
-df_tdms_dec_5kHz = WT_Noise_ChannelProcessor_averaging(tdms_raw_WT_5kHz[GROUP_NAME][CHAN_NAME]
+df_tdms_dec_5kHz = WT_Noise_ChannelProcessor_averaging.from_tdms(tdms_raw_WT_5kHz[GROUP_NAME][CHAN_NAME]
                 , desc= 'Inverter On, WS=0, 5kHz')
 
 # %%
-%matplotlib qt
-ds = df_tdms_inv_meas_1_0.raw_data()
-ds = df_tdms_inv_meas_1_0.
-fs_Hz = df_tdms_inv_meas_1_0.fs_Hz
-fr_Hz = 100
+# %matplotlib qt
+# ds = df_tdms_inv_meas_1_0.raw_data()
+# fs_Hz = df_tdms_inv_meas_1_0.fs_Hz
+# fr_Hz = 100
 
-ds_100 = ds.groupby(ds.index// (fs_Hz/fr_Hz)).mean()
+# ds_100 = ds.groupby(ds.index// (fs_Hz/fr_Hz)).mean()
 
-plt.plot(ds.index/fs_Hz, ds, '.', alpha = 0.3)
-plt.plot(ds_100.index/fr_Hz, ds_100, '.', alpha = 1)
+# plt.plot(ds.index/fs_Hz, ds, '.', alpha = 0.3)
+# plt.plot(ds_100.index/fr_Hz, ds_100, '.', alpha = 1)
 
+df_tdms_inv_meas_1_0_av100 = df_tdms_inv_meas_1_0.get_averaged(100)
+print(df_tdms_inv_meas_1_0.operations)
+print(df_tdms_inv_meas_1_0_av100.operations)
+print(df_tdms_inv_meas_1_0.get_decimated(1).operations)
+print(df_tdms_inv_meas_1_0.get_decimated(1).get_averaged(100).operations)
+print(df_tdms_inv_meas_1_0.filter_as_obj(fc_Hz=100).get_averaged(100).operations)
 # %%
-# %%
-# %%
+#TODO should create testing for class
+# print(df_tdms_inv_meas_1_0.fs_Hz)
+# print(df_tdms_inv_meas_1_0.data)
+# print(df_tdms_inv_meas_1_0.data_as_Series)
+print(df_tdms_inv_meas_1_0.filter(fc_Hz=100))
+# print(df_tdms_inv_meas_1_0.get_spectrum_filt(fc_Hz=100)) # graph obj
+# print(df_tdms_inv_meas_1_0.get_spectrum_raw_dec(dec=1)) # graph obj
+
+print(df_tdms_inv_meas_1_0.get_decimated(1).operations)
+# print(df_tdms_inv_meas_1_0.get_averaged(fr_Hz=100))   #-> Obj
 # %%
 # %%
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Up to here 
 # #%%
-# plot_spect_comb2([df_tdms_inv_meas_1_0.get_spectrum_raw_dec(dec=1)],
-#                 title='Raw signal at 100kHz',
-#                 xlim=[1e1,1e5], ylim = [1e-4,1e-2])
+plot_spect_comb2([df_tdms_inv_meas_1_0.get_spectrum_raw_dec(dec=1, nperseg=20*1024),
+                  df_tdms_inv_meas_1_0.get_spectrum_raw_dec(dec=2, nperseg=10*1024),
+                  df_tdms_inv_meas_1_0.get_spectrum_raw_dec(dec=20, nperseg=1024)
+                  ],
+                title='Comparison of decimated signal 100kHz',
+                xlim=[1e1,1e5], ylim = [1e-4,1e-0]
+                )
+#%% [markdown]
+# ## Comparing averaging after decimation  
+# comparing the signal 
+# when different filters are applied and averaging occurs
+#
+#  this is for ws0 so we may need to look at different WS (with and without)
+#%%
+plot_spect_comb2([df_tdms_inv_meas_1_0.get_averaged(fr_Hz=100).get_spectrum_raw_dec(dec=1, nperseg=1024),
+                  df_tdms_inv_meas_1_0.get_decimated(dec=2,offset=0).get_averaged(fr_Hz=100).get_spectrum_raw_dec(dec=1, nperseg=1024),
+                  df_tdms_inv_meas_1_0.get_decimated(dec=20,offset=0).get_averaged(fr_Hz=100).get_spectrum_raw_dec(dec=1, nperseg=1024)
+                  ],
+                title='Comparison of decimated signal 100kHz',
+                xlim=[1e0,1e4], ylim = [1e-8,1e-2]
+                )
+
+#%% [markdown]
+# ## Comparing averaging after filtering
+# comparing the signal 
+# when different filters are applied and averaging occurs
+#%%
+plot_spect_comb2([df_tdms_inv_meas_1_0.filter(fc_Hz = 100).get_averaged(fr_Hz=100).get_spectrum_raw_dec(dec=1, nperseg=1024),
+                  df_tdms_inv_meas_1_0.filter(fc_Hz = 200).get_averaged(fr_Hz=100).get_spectrum_raw_dec(dec=1, nperseg=1024),
+                  df_tdms_inv_meas_1_0.filter(fc_Hz = 2000).get_averaged(fr_Hz=100).get_spectrum_raw_dec(dec=1, nperseg=1024)
+                   ],
+                title='Comparison of decimated signal 100kHz',
+                xlim=[1e1,1e5], ylim = [1e-4,1e-0]
+                )
                 
-# #%%
+#%%
 # # %matplotlib inline
-# # %matplotlib qt
+# %matplotlib qt
 # # plot 50 kHz signals
 # plot_spect_comb2([df_tdms_inv_meas_1_0.get_spectrum_raw_dec(dec=2, offset=1),
 #                   df_tdms_dec_50kHz.get_spectrum_raw_dec(dec=1)
