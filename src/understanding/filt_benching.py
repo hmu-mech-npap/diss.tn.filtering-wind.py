834e63fa43200a723e642b5fac4675d616cb6f0e
Nick Torosian <goodvibrations32@protonmail.com>
2023-04-29 13:25:11 +0300

    make fir factory method play with main class .filter() function

--- src/understanding/filt_benching.py
+++ src/understanding/filt_benching.py
@@ -10,6 +10,7 @@ from nptdms import TdmsFile
 # import nptdms
 
 from pros_noisefiltering.WT_NoiProc import (filt_butter_factory,
+                                            fir_factory_constructor,
                                             plot_comparative_response)
 from pros_noisefiltering.WT_NoiProc import WT_NoiseChannelProc
 
@@ -154,111 +155,7 @@ dfi_i1_w20 = WT_NoiseChannelProc.from_tdms(
     l_tdms_Inv[5][GROUP_NAME][CHAN_NAME],
     desc='Inverter On, WS=20, 100kHz')
 
-
-fc_hz = 200
-
-
-# This addition is for the inferior python shell
-# for plotting the graphs using gtk app
-
-# Define a class for FIR operations like
-def fir_factory_constructor(fir_order=32, fc_Hz: float = 200):
-    """Mimicing so this is working with Papadakis solution above."""
-    def fir_filter(sig_r: np.ndarray,
-                   fs_hz: float, fc_hz_real: float = fc_Hz,
-                   fir_filt_order=fir_order):
-
-        fir_filt_coeff = signal.firwin(numtaps=fir_filt_order,
-                                       fs=fs_hz,
-                                       cutoff=fc_hz_real,
-                                       # pass_zero=False ,
-                                       # scale= True,
-                                       )
-        # # Hann approach
-        # fir_filt_coeff=signal.firwin(fir_order + 1,
-        #                              [0, 200/fs_hz],
-        #                              fs=fs_hz , window='hann')
-
-        # make output sos type to ensure normal operation
-        # this is crusial for elimination of ending ripples see image above
-        sos_fir_mode = signal.tf2sos(fir_filt_coeff, 1)
-        sos_filt_data = signal.sosfilt(sos_fir_mode, sig_r-sig_r[0])+sig_r[0]
-        warmup = fir_filt_order-1
-        uncorrupted_output = sos_filt_data[warmup:]
-        # filt_sig_time_int = time[warmup:]-((warmup/2)/fs_hz)
-        return uncorrupted_output           # uncorr_sos_output
-
-    # Add the parameter attribute for checking filter response
-    fir_filter.params = {'filter order': fir_order, 'fc_Hz': fc_Hz}
-    return fir_filter
-
-
 filter_fir_default = fir_factory_constructor(fir_order=2, fc_Hz=100)
-
-
-class Fir_filter:
-    """This class is used to take a signal as a tdms dataframe object.
-
-    (from pypkg funcs)
-    """
-
-    def __init__(self, signals) -> None:
-        """Initiate object constructor."""
-        self.raw = signals.data
-        # self.time_int = np.linspace(0, 7, len(self.raw))
-        self.description = signals.description
-        self._channel_data = signals._channel_data
-        self.fs_hz = int(1/signals._channel_data.properties['wf_increment'])
-        self.channel_name = signals._channel_data.name
-        self.time_int = np.linspace(0,
-                                    len(self.raw) / int(self.fs_hz),
-                                    len(self.raw))
-
-    def _fir_filter(self, fc_Hz: float = fc_hz,
-                    filter_func=filter_fir_default,
-                    fs_hz=None) -> pd.Series:
-        """#TODO UPDATE DOCSTRING: return a filtered signal based on.
-
-        Args:
-            fc_Hz (float): cut off frequency in Hz
-            filter_func : filtering function that
-                          takes two arguments (sig_r, fs_hz).
-                          Defaults to 100, filt_order = 2).
-            fs_hz (None): sampling frequency in Hz (is None in declaration)
-
-
-        Returns:
-            _type_: _description_
-        """
-        if fs_hz is not None:
-            logging.warning(
-                f'issued {fs_hz} sampling frequency \
-                while the default is {self.fs_Hz}')
-        fs_hz = fs_hz if fs_hz is not None else self.fs_hz
-        filtered = filter_func(sig_r=self.data, fs_hz=fs_hz, fc_Hz=fc_hz)
-        return pd.Series(filtered, name=f'{self.channel_name}:filt_fc_{fc_Hz}')
-
-    def fir_nested_filter(self,  fc_Hz: float,
-                          filter_func=filter_fir_default, fs_hz=None,
-                          desc=None):
-        """Construct a Wrapper for applying a filter."""
-        sig_r_filt = self._fir_filter(fc_Hz=fc_hz,
-                                      filter_func=filter_func,
-                                      fs_hz=fs_hz)
-        description = (
-            desc if desc is not None else self.description + f'_fc:{fc_Hz}')
-
-        return WT_NoiseChannelProc.from_obj(self,
-                                            desc=description,
-                                            data=sig_r_filt.values,
-                                            operation=f'pass filter {fc_Hz}'
-                                            )
-
-    # def apply_fir(self, fc_hz, order_for_filter: int):
-    #     filtered = fir_nested_filter(self.raw, fs_hz=self.fs_hz, fc_hz=fc_hz)
-    #     return pd.Series(filtered,
-    #                      name=f'{self.channel_name}:fir_filt_fc_{fc_hz}')
-
 
 fc_Hz = 200
 fir_or = 65
@@ -280,9 +177,9 @@ plot_comparative_response(df_tdms_1_10,             # cutoff frequency
                           Kolmogorov_offset=4e0,
                           nperseg=NPERSEG*100,
                           figsize=FIGSIZE_SQR_L)
-plt.savefig(
-    f'_temp_fig/s4-PS-i1-WS05-filt{filter_Butter_200.params.get("fc_Hz")}',
-    facecolor='white',
-    transparent=False)
+# plt.savefig(
+#     f'_temp_fig/s4-PS-i1-WS05-filt{filter_Butter_200.params.get("fc_Hz")}',
+#     facecolor='white',
+#     transparent=False)
 
 plt.show()
